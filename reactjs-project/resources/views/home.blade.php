546a2acdaf95451faf56c6476dff896ec76e7629
akku004 <rawatakku04@gmail.com>
2023-04-24 21:07:59 +0530

    create a table

--- reactjs-project/resources/views/home.blade.php
+++ reactjs-project/resources/views/home.blade.php
@@ -1,192 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="border">
-  <div class="row">
-      <div class="col"> 
-          SERIAL NO.
-      </div>
-
-      <div class="col">
-          IMAGES
-      </div>  
-
-      <div class="col">
-          BOOKNAME
-      </div>
-
-      <div class="col">  
-          BOOKTITLE
-      </div>
-
-      <div class="col">
-          AURTHOR NAME
-      </div>
-
-      <div class="col">
-          NO. OF BOOKS
-      </div>
-
-      <div class="col">
-          DESCRIPTION
-      </div>
-
-      <div class="col">
-          OPERATION 
-      </div>
-  </div>
-</div>
-  <div class="border">
-    <div class="row">
-      <div class="col">
-           1
-</div>
-<div class="col">
-           ---
-</div>
-<div class="col">
-           ---
-</div>
-<div class="col">
-           ---
-</div>
-<div class="col">
-           ---
-</div>
-<div class="col">
-           ---
-</div>
-<div class="col">
-           ---
-</div>
-<div class="col">
-           ---
-</div>
-<div class="col">
-           ---
-      </div>
-      <div class="row">
-      <div class="col">
-           2
-      </div>
-      <div class="col">
-           ---
-      </div>
-       <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="row">
-      <div class="col">
-           3
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="row">
-      <div class="col">
-           4
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="row">
-      <div class="col">
-           5
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-      <div class="col">
-           ---
-      </div>
-
-</div> 
-   </div>
-    </div>
-  </div>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">S NO.</th>
+      <th scope="col">BOOKNAME</th>
+      <th scope="col">AURTHOR NAME</th>
+      <th scope="col">NO. OF BOOK</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>---</td>
+      <td>---</td>
+      <td>---</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>---</td>
+      <td>---</td>
+      <td>---</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>---</td>
+      <td>---</td>
+      <td>---</td>
+    </tr>
+  </tbody>
+</table>
             
 @endsection
