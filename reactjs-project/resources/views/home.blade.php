a8a6cbb42d7187d3d4ec976d007b6f9e13e1e594
akku004 <rawatakku04@gmail.com>
2023-04-24 20:53:57 +0530

    create a table

--- reactjs-project/resources/views/home.blade.php
+++ reactjs-project/resources/views/home.blade.php
@@ -1,62 +1,192 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-<div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">
-                    <div align="right">{{ __('Name') }}</div>
-                </div>
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                        <div class="ml-3">
-                    {{ __('Akash Rawat') }}
-                </div>
-            </div>
-        </div>
+<div class="border">
+  <div class="row">
+      <div class="col"> 
+          SERIAL NO.
+      </div>
+
+      <div class="col">
+          IMAGES
+      </div>  
+
+      <div class="col">
+          BOOKNAME
+      </div>
+
+      <div class="col">  
+          BOOKTITLE
+      </div>
+
+      <div class="col">
+          AURTHOR NAME
+      </div>
+
+      <div class="col">
+          NO. OF BOOKS
+      </div>
+
+      <div class="col">
+          DESCRIPTION
+      </div>
+
+      <div class="col">
+          OPERATION 
+      </div>
+  </div>
+</div>
+  <div class="border">
+    <div class="row">
+      <div class="col">
+           1
+</div>
+<div class="col">
+           ---
+</div>
+<div class="col">
+           ---
+</div>
+<div class="col">
+           ---
+</div>
+<div class="col">
+           ---
+</div>
+<div class="col">
+           ---
+</div>
+<div class="col">
+           ---
+</div>
+<div class="col">
+           ---
+</div>
+<div class="col">
+           ---
+      </div>
+      <div class="row">
+      <div class="col">
+           2
+      </div>
+      <div class="col">
+           ---
+      </div>
+       <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="row">
+      <div class="col">
+           3
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="row">
+      <div class="col">
+           4
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="row">
+      <div class="col">
+           5
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+      <div class="col">
+           ---
+      </div>
+
+</div> 
+   </div>
     </div>
-</div>
-
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">
-                    <div align="right">{{ __('Dashboard') }}</div>
-                </div>
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                        <div align="right">
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="card">
-    <form>
-        <div class="card-header">
-            <h3>Sign Up<h3>
-            <p> Please Fill This Form To Create An Account <p>
-        </div>
-            <h5>Name<h5>
-            <h5><input type="text" placeholder="Name"/><h5>
-            <h5>Email<h5>
-            <h5><input type="Email" placeholder="Email"/><h5>
-    </form>
-</div>
-
-
-
-  
-
+  </div>
             
 @endsection
