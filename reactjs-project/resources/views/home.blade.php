f51c1e7970d51b798e631269ad68cd4deea3979d
LavishRawat <lavish.rawat@coloredcow.in>
2023-05-08 21:08:00 +0530

    minor changes

--- reactjs-project/resources/views/home.blade.php
+++ reactjs-project/resources/views/home.blade.php
@@ -1,35 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">S NO.</th>
-      <th scope="col">BOOKNAME</th>
-      <th scope="col">AURTHOR NAME</th>
-      <th scope="col">NO. OF BOOK</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>---</td>
-      <td>---</td>
-      <td>---</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>---</td>
-      <td>---</td>
-      <td>---</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>---</td>
-      <td>---</td>
-      <td>---</td>
-    </tr>
-  </tbody>
-</table>
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        </div>
+      </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
             
 @endsection
